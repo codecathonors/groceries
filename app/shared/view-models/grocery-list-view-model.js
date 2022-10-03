6cee912339f665ac3766b67eeb56cb0d226a97dd
CatherineHonors <cathonors@gmail.com>
2022-10-03 11:05:13 -0400

    downloading code

---
+++ app/shared/view-models/grocery-list-view-model.js
@@ -0,0 +1,28 @@
+var config = require("../../shared/config");
+var ObservableArray = require("tns-core-modules/data/observable-array").ObservableArray;
+
+function GroceryListViewModel(items) {
+    var baseUrl = config.apiUrl + "appdata/" + config.appKey + "/Groceries";
+    var viewModel = new ObservableArray(items);
+
+
+
+    return viewModel;
+}
+
+function getCommonHeaders() {
+    return {
+        "Content-Type": "application/json",
+        "Authorization": "Kinvey " + config.token
+    }
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        console.log(JSON.stringify(response));
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+module.exports = GroceryListViewModel;
